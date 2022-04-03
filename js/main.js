f1fb9675dd66e3dbdae6b016887a894609ce4d5b
ashishbhat21 <98099574+ashishbhat21@users.noreply.github.com>
2022-04-02 20:25:52 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
+let countFree = 0;
+let countPaid = 0;
+let totalFree = 0;
+let totalPaid = 0;
+let myPieChart;
+let myPieChart2;
 
+d3.csv("data/data.csv", (row) => {
+    // convertrow.value = +row.value
+    if (row.Type == "Free" && row.Installs >= 10000000) {
+        countFree += 1;
+        return {
+            installs: +row.Installs,
+            type: row.Type
+        };
+    }
+    else if (row.Type == "Free" && row.Installs < 10000000) {
+        totalFree += 1;
+        return {
+            installs: +row.Installs,
+            type: row.Type
+        };
+    }
+    else if (row.Type == "Paid" && row.Installs >= 10000000) {
+        countPaid += 1;
+        return {
+            installs: +row.Installs,
+            type: row.Type
+        };
+    }
+    else if (row.Type == "Paid" && row.Installs < 10000000) {
+        totalPaid += 1;
+        return {
+            installs: +row.Installs,
+            type: row.Type
+        };
+    }
+}).then((data) => {
 
-d3.csv("data/**").then(data => {
-    // test commit
-    // adding comment
-});
+    myPieChart = new PieChart('pieDivRight', "Free Applications", countFree, totalFree);
 
+    myPieChart2 = new PieChart('mapDiv', "Paid Applications", countPaid, totalPaid);
 
+})
