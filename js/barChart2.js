30979f06e0deaddfb156d654d2d4d9e73a8c7344
Zhen Shen <zeroshen@vt.edu>
2022-04-23 19:18:41 -0400

    Fix some problems

--- js/barChart2.js
+++ js/barChart2.js
@@ -16,7 +16,7 @@ class BarChart2 {
 
         // SVG drawing area
 
-        vis.margin = { top: 50, right: 30, bottom: 70, left: 60 };
+        vis.margin = { top: 50, right: 30, bottom: 90, left: 60 };
 
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right,
             vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
@@ -94,7 +94,7 @@ class BarChart2 {
 
         vis.svg.append("text")      // text label for the x axis
             .attr("x", 300 )
-            .attr("y", 370 )
+            .attr("y", 395 )
             .attr("font-weight", "bold")
             // .attr("transform", "translate(-80) rotate(-90)")
             .style("text-anchor", "middle")
