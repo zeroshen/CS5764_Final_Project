fe9211e1f19e96256e5c9fea84c6c8e397c3f225
Ashish Bhat <53263263+ashishbhat99@users.noreply.github.com>
2022-05-01 15:15:49 -0400

    Ignore the visualizations for now

--- js/pieChart.js
+++ js/pieChart.js
@@ -23,7 +23,7 @@ class PieChart {
         let vis = this;
 
         // margin conventions
-        vis.margin = {top: 10, right: 50, bottom: 10, left: 50};
+        vis.margin = {top: 10, right: 0, bottom: 10, left: 0};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
